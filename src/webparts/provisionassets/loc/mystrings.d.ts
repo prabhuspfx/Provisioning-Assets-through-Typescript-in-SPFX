57a1da59773e9b8b14661f33503c6f587da6fd2e
prabhuspfx <54140444+prabhuspfx@users.noreply.github.com>
2019-08-23 16:37:03 +0530

    Initial commit

---
+++ src/webparts/provisionassets/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IProvisionassetsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ProvisionassetsWebPartStrings' {
+  const strings: IProvisionassetsWebPartStrings;
+  export = strings;
+}
